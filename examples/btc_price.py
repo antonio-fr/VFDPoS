58b439ed3ab5d4d23970ab30d1eaa29a2b7737f0
antonio-fr <anferron@gmail.com>
2015-06-29 01:29:07 +0200

    Clock improved to a real-time display
    Some Examples changed to basic 2 lines VFD
    Add license on examples

--- examples/btc_price.py
+++ examples/btc_price.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+# Example for VFDPoS library for WN VFD
+# Displays Bitcoin Price in real-time
+# Copyright (C) 2015  Antoine FERRON
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 import time
 from vfdpos import *
 from easy_rest_json import * 
 
-
-mypos = vfd_pos(0x0201)
+mypos = vfd_pos(0x0200)
 price_api = rest_json('https://api.coindesk.com/v1/bpi/currentprice.json')
-mypos.write_msg("Bitcoin Price")
+
 print "PRESS CTRL+C TO QUIT"
 try:
 	while True:
 		price_api.get_data()
 		btc_price = float( price_api.getkey("bpi/USD/rate") )
-		mypos.poscur(3,0)
-		mypos.write_msg("1 BTC = %.2f USD  "%btc_price)
+		mypos.poscur(0,0)
+		mypos.write_msg("Bitcoin Price\r\n 1 BTC = %.2f USD  "%btc_price)
 		time.sleep(60)
 except KeyboardInterrupt:
 	pass
--- examples/clock.py
+++ examples/clock.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+# Example for VFDPoS library for WN VFD
+# Displays date and time
+# Copyright (C) 2015  Antoine FERRON
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 import vfdpos
 import time
 
-wnpos = vfdpos.vfd_pos(0x0201)
+wnpos = vfdpos.vfd_pos(0x0200)
 print "PRESS CTRL+C TO QUIT"
+
+def printdate(onepos):
+		present_time = time.localtime()
+		onepos.poscur(1,1)
+		remaining = 60 - present_time.tm_sec
+		date = time.strftime("%a %d %b %Y", present_time).center(20)
+		onepos.write_msg(date)
+		hour = time.strftime("%H:%M", present_time).center(20)
+		onepos.poscur(2,1)
+		onepos.write_msg(hour)
+		time.sleep(remaining)
+
 try:
 	while True:
-		wnpos.poscur(1,1)
-		prestime = time.localtime()
-		date = time.strftime("%a %d %b %Y", prestime).center(20)
-		wnpos.write_msg(date)
-		hour = time.strftime("%H:%M", prestime).center(20)
-		wnpos.poscur(3,1)
-		wnpos.write_msg(hour) 
-		time.sleep(10)
+		printdate(wnpos)
 except KeyboardInterrupt:
 	pass
 wnpos.close()
